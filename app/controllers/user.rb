67d63111448ce0c09bede0f16a4b3d9ff29a06d9
Vivek Ratkalkar <vratkalkar@live.com>
2015-02-24 20:55:59 -0500

    Started changes to file directory

---
+++ app/controllers/user.rb
@@ -0,0 +1,20 @@
+get '/user' do
+end
+
+get '/user/new' do
+end
+
+post '/user' do
+end
+
+get '/user/:id' do
+end
+
+get '/user/:id/edit' do
+end
+
+put '/user/:id' do
+end
+
+delete '/user/:id' do
+end
