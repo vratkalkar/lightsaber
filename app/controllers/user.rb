bff43093c4ed1f05a094a851bfe9f076559296b5
Vivek Ratkalkar <vratkalkar@live.com>
2015-02-25 10:39:23 -0500

    Remove erbs from routes

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,18 +2,15 @@ get '/user' do
 end
 
 get '/user/new' do
-  erb :'user/new'
 end
 
 post '/user' do
 end
 
 get '/user/:id' do
-  erb :'user/show'
 end
 
 get '/user/:id/edit' do
-  erb :'user/edit'
 end
 
 put '/user/:id' do
