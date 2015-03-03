2a5fc7e019685fd9ff4377efe4da028a25a59b0b
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-03 15:38:44 -0500

    Added jasmine tests to spec folder

---
+++ spec/javascripts/calculator-spec.js
@@ -0,0 +1,23 @@
+describe("Calculator Add", function() {
+  it("should sum two numbers", function() {
+    expect(calculator.add(4,2)).toEqual(6);
+  });
+});
+
+describe("Calculator Subtract", function() {
+  it("should subtract two numbers", function() {
+    expect(calculator.sub(4,2)).toEqual(2);
+  });
+});
+
+describe("Calculator Divide", function() {
+  it("should divide two numbers", function() {
+    expect(calculator.add(4,2)).toEqual(2);
+  });
+});
+
+describe("Calculator Multiply", function() {
+  it("should sum two numbers", function() {
+    expect(calculator.add(4,2)).toEqual(8);
+  });
+});
