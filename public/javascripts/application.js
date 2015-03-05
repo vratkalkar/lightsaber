8d5d66460abae6cfc6860eae14b6ffd210fd3746
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-05 18:32:01 -0500

    Added comments to code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,11 +4,14 @@
 $(document).ready(function() {
 
   var Number = (function(){
+
+    // This is a private variable and cannot be called outside the function.
     var start = 0;
     console.log(start);
 
     return {
 
+      // These are public functions calling the private variable and return the start value.
       add: function(){
         start++;
         console.log(start);
