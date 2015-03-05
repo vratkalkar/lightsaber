254faa672f15fa31e0ce459efd4396a8ffa2fecd
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-04 20:25:31 -0500

    Added person constructor and created object

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Javascript Goes Here~
 //
+
+function Person(name,email,phone){
+  this.name = name,
+  this.email = email,
+  this.phone = phone
+};
+
+var vivek = new Person("Vivek", "vr@email.com", "732-995-1487");
