b1a4e4b3a2dd00c2fa3a2a256166361eb5ee3b14
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-05 18:23:09 -0500

    Add Number module to application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
 // Javascript Goes Here~
 //
+
+$(document).ready(function() {
+
+  var Number = (function(){
+    var start = 0;
+    console.log(start);
+
+    return {
+
+      add: function(){
+        start++;
+        console.log(start);
+      },
+      reset: function(){
+         start--;
+         console.log(start);
+      }
+    };
+
+ }());
+
+});
+
